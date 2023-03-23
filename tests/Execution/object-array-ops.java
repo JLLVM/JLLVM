99d0fe7a573f328a25bc1fb5a833a3a370bcf981
zero9178 <markus.boeck02@gmail.com>
2023-03-23 14:33:24 +0100

    [test] add regression test for object array access
    I broke these while working on another PR but there were no tests failing, clearly indicating lack of test coverage.
    
    Note, due to a different bug somewhat related to https://github.com/JLLVM/JLLVM/issues/23, we can't currently use `for` loops for the initialization.

---
+++ tests/Execution/object-array-ops.java
@@ -0,0 +1,35 @@
+// RUN: javac %s -d %t
+// RUN: jllvm -Xenable-test-utils %t/Test.class | FileCheck %s
+
+class Test
+{
+    public static native void print(int i);
+
+	public int value;
+
+	public Test(int value)
+	{
+		this.value = value;
+	}
+
+    public static void main(String[] args)
+    {
+        var is = new Test[3];
+		is[0] = new Test(0);
+		is[1] = new Test(1);
+		is[2] = new Test(2);
+
+		// Something random inbetween, maybe GCs.
+		new Object();
+
+		print(is[0].value);
+		print(is[1].value);
+		print(is[2].value);
+		print(is.length);
+    }
+}
+
+// CHECK: 0
+// CHECK: 1
+// CHECK: 2
+// CHECK: 3
