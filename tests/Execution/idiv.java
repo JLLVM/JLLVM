312567432872a30c4af0ea71f9de5ec6ee781955
Thomas Mayerl <thomas.mayerl@tuwien.ac.at>
2023-03-30 14:19:21 +0200

    [instruction] Add more tests for idiv

--- tests/Execution/idiv.java
+++ tests/Execution/idiv.java
@@ -10,14 +10,26 @@ class Test
         int x = 3;
         int y = 2;
         // CHECK: 1
+        // CHECK-NOT:-
         print(x/y);
         // CHECK: 3
+        // CHECK-NOT:-
         print(x/1);
         // CHECK: 9
+        // CHECK-NOT:-
         print(27/x);
         // CHECK: 9
+        // CHECK-NOT:-
         print(28/x);
         // CHECK: 0
+        // CHECK-NOT:-
         print(Integer.MAX_VALUE/Integer.MIN_VALUE);
+        // CHECK: -3
+        print(x/-1);
+        // CHECK: 0
+        // CHECK-NOT:-
+        print(-1/x);
+        // CHECK: -5
+        print(-15/3);
     }
 }
