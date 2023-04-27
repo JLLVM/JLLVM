19c3a9473bcd9733dca269f3fb3f1ec5db51b0af
ThomasMayerl <thomas.mayerl@tuwien.ac.at>
2023-04-27 22:02:25 +0200

    [instruction] Implement irem instruction (#50)
    This PR includes support for creating appropriate LLVM instructions on
    processing the irem JVM instruction.
    
    fixes https://github.com/JLLVM/JLLVM/issues/49

---
+++ tests/Execution/irem.java
@@ -0,0 +1,27 @@
+// RUN: javac %s -d %t
+// RUN: jllvm -Xenable-test-utils %t/Test.class | FileCheck %s
+
+class Test
+{
+    public static native void print(int i);
+
+    public static void main(String[] args)
+    {
+        int x = 3;
+        int y = 2;
+        int a = -40;
+        int b = 5601;
+        // CHECK: 1
+        print(x%y);
+        // CHECK: 0
+        print(x%1);
+        // CHECK: 0
+        print(27%x);
+        // CHECK: 1
+        print(28%x);
+        // CHECK: -40
+        print(a%b);
+        // CHECK: 1
+        print(b%a);
+    }
+}
