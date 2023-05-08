58e804a6e11660b4cab0a7650290301a3d730e30
Thomas Mayerl <thomas.mayerl@tuwien.ac.at>
2023-05-08 21:21:59 +0200

    [instruction] Implement int shifting instructions

---
+++ tests/Execution/int-shifts.java
@@ -0,0 +1,35 @@
+// RUN: javac %s -d %t
+// RUN: jllvm -Xenable-test-utils %t/Test.class | FileCheck %s
+
+class Test
+{
+    public static native void print(int i);
+
+    public static void main(String[] args)
+    {
+        int x = 5;
+        int y = 2;
+        int z = -1;
+
+        //CHECK: 20
+        print(x << y);
+        //CHECK: 64
+        print(y << x);
+        //CHECK: -4
+        print(z << y);
+
+        //CHECK: 1
+        print(x >> y);
+        //CHECK: 0
+        print(y >> x);
+        //CHECK: -1
+        print(z >> y);
+
+        //CHECK: 1
+        print(x >>> y);
+        //CHECK: 0
+        print(y >>> x);
+        //CHECK: 1073741823
+        print(z >>> y);
+    }
+}
