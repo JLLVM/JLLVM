dd1e6681bf7c508903e79325b881d80eb77fea68
Nemegyeb <nemegyeb@gmail.com>
2023-05-17 22:57:41 +0200

    [instruction] Add support for fconst instructions (#4)

--- tests/Execution/jni-primitives.java
+++ tests/Execution/jni-primitives.java
@@ -4,10 +4,13 @@
 class Test
 {
     public static native void print(byte i);
-    public static native void print(short i);
     public static native void print(char i);
+    public static native void print(double i);
+    public static native void print(float i);
     public static native void print(int i);
     public static native void print(long i);
+    public static native void print(short i);
+    public static native void print(boolean i);
 
     public static void main(String[] args)
     {
@@ -25,8 +28,18 @@ class Test
         // Java char does not support assignment from negative values
         char c = 5;
         print(5);
+
+        print(true);
+
+        print(0.0f);
+        print(1.0f);
+        print(2.0f);
     }
 }
 
 // CHECK-COUNT-3: -1
 // CHECK: 5
+// CHECK: 1
+// CHECK: 0
+// CHECK: 1
+// CHECK: 2
