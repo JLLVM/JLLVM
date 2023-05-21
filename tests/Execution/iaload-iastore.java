b343f6c3434c5f971d56c89d95bb816207b914d9
ThomasMayerl <thomas.mayerl@tuwien.ac.at>
2023-05-21 11:10:51 +0200

    [instruction] Implement newarray, iaload and iastore instructions (#78)
    This PR includes support for creating appropriate LLVM instructions on
    processing the newarray, iaload and iastore instructions.
    
    fixes https://github.com/JLLVM/JLLVM/issues/77

---
+++ tests/Execution/iaload-iastore.java
@@ -0,0 +1,27 @@
+// RUN: javac %s -d %t
+// RUN: jllvm -Xenable-test-utils %t/Test.class | FileCheck %s
+
+class Test
+{
+    public static native void print(int i);
+
+    public static void main(String[] args)
+    {
+        int x = 3;
+        int y = 2;
+        int z = 1;
+        int a = -10;
+
+        int[] arr = {x, y, z, a};
+        // CHECK: 3
+        print(arr[0]);
+        // CHECK: 2
+        print(arr[1]);
+        // CHECK: 1
+        print(arr[2]);
+        // CHECK: -10
+        print(arr[3]);
+        // CHECK: 4
+        print(arr.length);
+    }
+}
