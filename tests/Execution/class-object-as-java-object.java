fca3b0fd3c0498e83f6edad0f3dd923eb912c58e
zero9178 <markus.boeck02@gmail.com>
2023-05-21 09:29:03 +0200

    use boolean overload of print
    somehow forgot we had these

--- tests/Execution/class-object-as-java-object.java
+++ tests/Execution/class-object-as-java-object.java
@@ -3,22 +3,22 @@
 
 class Test
 {
-    public static native void print(int i);
+    public static native void print(boolean i);
 
     public static void main(String[] args)
     {
         var clazz = Object.class.getClass();
         // CHECK: 1
-        print(clazz == clazz.getClass() ? 1 : 0);
+        print(clazz == clazz.getClass());
 
         var array = Object[].class;
         // CHECK: 1
-        print(array.isArray() ? 1 : 0);
+        print(array.isArray());
         // CHECK: 1
-        print(array.getComponentType() == Object.class ? 1 : 0);
+        print(array.getComponentType() == Object.class);
 
         var o = new Object();
         // CHECK: 1
-        print(o.getClass() == Object.class ? 1 : 0);
+        print(o.getClass() == Object.class);
     }
 }
