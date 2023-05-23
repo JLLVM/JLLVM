2f2e7d535f5a61bbc983eedb21d4f74f96a1ffd0
zero9178 <markus.boeck02@gmail.com>
2023-05-23 18:46:13 +0200

    [vm] implement basic implementation for 'assert' class initializers
    Any uses of `assert` within a class leads to the creation of a class initializer which queries the assertion status from the class loader and VM: https://godbolt.org/z/9a6q675bx
    
    This PR implements the 'native' method called from `Class.desiredAssertionStatus` to allow our VM to load class files that contain `assert` (even if they're never executed). `assert`s themselves do not yet work (nor can be compiled) since it contains `athrow` of `AssertionError`.
    
    I currently made it simply match whether the VM is compiled in debug or release but this isn't really proper. Rather, a command line option should be added in the future to make this user configurable, but this is beyond the scope of this PR

---
+++ tests/Execution/assert.java
@@ -0,0 +1,17 @@
+// RUN: javac %s -d %t
+// RUN: jllvm -Xenable-test-utils %t/Test.class
+
+class Test
+{
+    public static int x;
+
+    private void dummy()
+    {
+        assert x == 5;
+    }
+
+    public static void main(String[] args)
+    {
+        // Nothing for now, just making sure the implicit '<clinit>' works
+    }
+}
