370a63655811fa0985227701165e6d2d94459b1c
nemegyeb <80166690+nemegyeb@users.noreply.github.com>
2023-05-23 20:28:35 +0000

    [instruction] Add support for `ldc_w` and `ldc2_w` instructions (#99)
    This PR includes support for creating appropriate LLVM instructions on
    processing the `ldc_w` and `ldc2_w` JVM instructions.
    
    Fixes: #17

---
+++ tests/Execution/ldc2_w.java
@@ -0,0 +1,29 @@
+// RUN: javac %s -d %t
+// RUN: jllvm -Xenable-test-utils %t/Test.class | FileCheck %s
+class Test
+{
+    public static native void print(double d);
+    public static native void print(long l);
+
+    public static void main(String[] args)
+    {
+
+        // CHECK: 123.456
+        print(123.456);
+        // CHECK: -987.654
+        print(-987.654);
+
+        // larger double than Float.MAX_VALUE
+        // CHECK: 4.40282e+38
+        print(4.4028235E38);
+
+        // CHECK: 123456
+        print(123456l);
+        // CHECK: 987654
+        print(987654l);
+
+        // larger long than Integer.MAX_VALUE
+        // CHECK: 12147483647
+        print(12147483647l);
+    }
+}
