1a10def0110519fd2f04576bf1095a6ccbb8bc93
ThomasMayerl <thomas.mayerl@tuwien.ac.at>
2023-06-05 11:30:10 +0200

    [instruction] Implement lshl, lshr and lushr instructions (#132)
    This PR includes support for creating appropriate LLVM instructions on
    processing the lshl, lshr and lushr JVM instructions.
    
    fixes https://github.com/JLLVM/JLLVM/issues/131

---
+++ tests/Execution/long-shifts.java
@@ -0,0 +1,44 @@
+// RUN: javac %s -d %t
+// RUN: jllvm -Xenable-test-utils %t/Test.class | FileCheck %s
+
+class Test
+{
+    public static native void print(long i);
+
+    public static void main(String[] args)
+    {
+        long x = 5l;
+        long y = 2l;
+        long z = -1l;
+        long min = Long.MIN_VALUE;
+        long max = Long.MAX_VALUE;
+
+        //CHECK: 20
+        print(x << 2);
+        //CHECK: 64
+        print(y << 5);
+        //CHECK: -4
+        print(z << 2);
+
+        //CHECK: 1
+        print(x >> 2);
+        //CHECK: 0
+        print(y >> 5);
+        //CHECK: -1
+        print(z >> 2);
+
+        //CHECK: 1
+        print(x >>> 2);
+        //CHECK: 0
+        print(y >>> 5);
+        //CHECK: 4611686018427387903
+        print(z >>> 2);
+
+        //CHECK: 2305843009213693952
+        print(min >>> 2);
+        //CHECK: -2305843009213693952
+        print(min >> 2);
+        //CHECK: 4
+        print(max << 2);
+    }
+}
