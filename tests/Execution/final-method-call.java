e92e8f02c9cfe562b6ad416e8e882f9d3ea0bdec
zero9178 <markus.boeck02@gmail.com>
2023-06-16 11:29:59 +0200

    [object][NFC] reduce number of VTable slots
    Since our new `invokevirtual` implementation it is now perfectly possible to create direct calls to methods if after resolution we know they can't be overwritten. This is the case for any private or final instance methods for example.
    
    This PR uses that capability to reduce the amount of V-Table slots we create for class objects.

---
+++ tests/Execution/final-method-call.java
@@ -0,0 +1,32 @@
+// RUN: rm -rf %t && split-file %s %t
+// RUN: cd %t && javac %t/Other.java -d %t
+// RUN: jllvm -Xenable-test-utils %t/Other.class | FileCheck %s
+
+//--- Test.java
+
+public class Test
+{
+    public static native void print(int i);
+}
+
+//--- A.java
+
+public class A
+{
+    public final void a()
+    {
+        Test.print(5);
+    }
+}
+
+//--- Other.java
+
+public class Other extends A
+{
+    public static void main(String[] args)
+    {
+        Other c = new Other();
+        // CHECK: 5
+        c.a();
+    }
+}
