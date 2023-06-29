099d7e167494292629f39a7d33d173baeac78982
zero9178 <markus.boeck02@gmail.com>
2023-06-29 17:45:05 +0200

    Switch to GPLv3

--- src/jllvm/materialization/ByteCodeLayer.hpp
+++ src/jllvm/materialization/ByteCodeLayer.hpp
@@ -1,3 +1,16 @@
+// Copyright (C) 2023 The JLLVM Contributors.
+//
+// This file is part of JLLVM.
+//
+// JLLVM is free software; you can redistribute it and/or modify it under  the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3, or (at your option) any later version.
+//
+// JLLVM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with JLLVM; see the file LICENSE.txt.  If not
+// see <http://www.gnu.org/licenses/>.
+
 #pragma once
 
 #include <llvm/ExecutionEngine/Orc/Mangling.h>
