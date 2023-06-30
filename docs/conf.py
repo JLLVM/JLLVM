17c17e60aba31dbaa11bd64f71617f7c9ff03a7e
zero9178 <markus.boeck02@gmail.com>
2023-06-30 18:01:27 +0200

    [docs] Add initial documentation
    This PR is an initial draft at adding a deployable config for us to write any project documentation.
    
    The documentation system proposed is using Sphinx together with MyST.
    The advantage of Sphinx is that it is extendable via plugins, has a rich ecosystem and also produces static HTML files that can be viewed offline without a running webserver.
    
    The advantage of MyST is that for 90% of tasks, one can simply use normal markdown, which, in my opinion, is more comfortable than RST.
    Yet when more advanced features of RST are required, it is possible to use these as well using special syntax.
    See https://mystmd.org/ as introduction
    
    Last but not least, I used https://github.com/pradyunsg/furo as a theme because I think it looks cool, has an automatic dark mode and a nice TOC.
    
    A GitHub pages workflow has been added for automatically deploying the webpage as well.

---
+++ docs/conf.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2023 The JLLVM Contributors.
+#
+# This file is part of JLLVM.
+#
+# JLLVM is free software; you can redistribute it and/or modify it under  the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3, or (at your option) any later version.
+#
+# JLLVM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with JLLVM; see the file LICENSE.txt.  If not
+# see <http://www.gnu.org/licenses/>.
+
+import os.path
+import sys
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'JLLVM'
+copyright = '2023, The JLLVM Contributors'
+author = 'The JLLVM Contributors'
+
+# -- General configuration ---------------------------------------------------
+
+sys.path.append(os.path.abspath('./_ext'))
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'myst_parser'
+]
+
+myst_enable_extensions = ['dollarmath']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
