b37360d15efaa88c0cc1fc114a8a1f46fb775e63
Markus Böck <markus.boeck02@gmail.com>
2023-11-23 09:14:12 +0100

    fix incorrect license header

--- src/jllvm/materialization/JIT2InterpreterLayer.cpp
+++ src/jllvm/materialization/JIT2InterpreterLayer.cpp
@@ -1,6 +1,15 @@
-// Licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (C) 2023 The JLLVM Contributors.
+//
+// This file is part of JLLVM.
+//
+// JLLVM is free software; you can redistribute it and/or modify it under  the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3, or (at your option) any later version.
+//
+// JLLVM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with JLLVM; see the file LICENSE.txt.  If not
+// see <http://www.gnu.org/licenses/>.
 
 #include "JIT2InterpreterLayer.hpp"
 
