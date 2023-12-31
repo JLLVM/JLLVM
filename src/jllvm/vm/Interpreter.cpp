1184a3adc2071c7bab03bb0af3e423941ba162ac
Markus Böck <markus.boeck02@gmail.com>
2023-12-31 08:51:31 +0100

    [interpreter] Reuse single interpreter entry in `JIT2InterpreterLayer`
    This layer has so far generated unnecessarily large amounts of LLVM IR by needlessly compiling a new interpreter entry each time.
    
    This PR simplifies its implementation by reusing the single interpreter entry created previously. The layer is now only responsible for translating from the JIT CC to the interpreter CC. This sadly still has to happen per-method rather than per-method-type (as in the interpreter2jit translation) as we require method objects to be prepended, but JIT -> Interpreter calls are expected not to be hot anyway.
    
    After other PRs which rework the interpreter OSR mechanism land the InterpreterEntry.cpp file can be deleted.

--- src/jllvm/vm/Interpreter.cpp
+++ src/jllvm/vm/Interpreter.cpp
@@ -27,11 +27,13 @@ jllvm::Interpreter::Interpreter(VirtualMachine& virtualMachine, bool enableOSR)
                                   virtualMachine.getRuntime().getLLVMIRLayer(),
                                   virtualMachine.getRuntime().getDataLayout())
 {
+    m_interpreterCCSymbols.addToLinkOrder(virtualMachine.getRuntime().getCLibDylib());
+    m_jit2InterpreterSymbols.addToLinkOrder(m_interpreterCCSymbols);
     m_jit2InterpreterSymbols.addToLinkOrder(virtualMachine.getRuntime().getClassAndMethodObjectsDylib());
     m_jit2InterpreterSymbols.addToLinkOrder(virtualMachine.getRuntime().getCLibDylib());
 
     m_virtualMachine.getRuntime().addImplementationSymbols(
-        m_jit2InterpreterSymbols,
+        m_interpreterCCSymbols,
         std::pair{"jllvm_interpreter",
                   [&](const Method* method, std::uint16_t* byteCodeOffset, std::uint16_t* topOfStack,
                       std::uint64_t* operandStack, std::uint64_t* operandGCMask, std::uint64_t* localVariables,
@@ -76,9 +78,6 @@ jllvm::Interpreter::Interpreter(VirtualMachine& virtualMachine, bool enableOSR)
             }},
         std::pair{"jllvm_osr_frame_delete", [](const std::uint64_t* osrFrame) { delete[] osrFrame; }});
 
-    // Reuse the linking order of the JIT CC interpreter entries for now.
-    m_jit2InterpreterSymbols.withLinkOrderDo([&](const llvm::orc::JITDylibSearchOrder& linkOrder)
-                                             { m_interpreterCCSymbols.setLinkOrder(linkOrder); });
     generateInterpreterEntry();
     m_interpreterInterpreterCCOSREntry = generateOSREntry("V", CallingConvention::Interpreter);
     m_interpreterJITCCOSREntryReferenceReturn = generateOSREntry("Ljava/lang/Object;", CallingConvention::JIT);
